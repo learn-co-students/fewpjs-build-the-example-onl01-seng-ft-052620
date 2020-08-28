7ad755a193b8fdb4bc03226c278aba894ac000d7
charlie763 <cwisoff@gmail.com>
2020-08-28 09:42:21 -0600

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', ()=>{
+  likeHearts = document.querySelectorAll('.like-glyph');
+  
+  function handleError(error){
+    errorModal = document.querySelector('#modal');
+    errorMessage = document.querySelector('#modal p#modal-message');
+    errorMessage.textContent = `${error}`;
+    errorModal.classList.remove('hidden');
+    window.setTimeout(()=> {
+      errorModal.classList.add('hidden');
+    }, 5000);
+  }
 
+  function handleSuccess(heart){
+    if ([... heart.classList.values()].includes('activated-heart')){
+      heart.classList.remove('activated-heart');
+      heart.textContent = EMPTY_HEART;
+    } else {
+      heart.classList.add('activated-heart');
+      heart.textContent = FULL_HEART;
+    }
+  }
+
+  for (const heart of likeHearts){
+    heart.addEventListener('click', (e)=>{
+      const request = mimicServerCall();
+      request.catch((error) => handleError(error))
+        .then((success) => {
+          if (success){handleSuccess(e.target)};
+        });
+    })
+  }
+});
 
 
 
