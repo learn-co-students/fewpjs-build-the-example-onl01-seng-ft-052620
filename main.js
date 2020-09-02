c7d0d73da092b5edfd0178da4576db7c98a22e86
jessegan <jesse13579@gmail.com>
2020-09-02 11:24:35 -0400

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,34 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", function(){
 
+  document.querySelectorAll(".like-glyph").forEach(heart => heart.addEventListener("click", function(e) {
+    if (e.target.innerHTML == EMPTY_HEART) {
+      mimicServerCall()
+        .then(function(obj) {
+          e.target.innerHTML = FULL_HEART
+          e.target.classList.add("activated-heart")
+        })
+        .catch(function(error){
+          console.log(error)
+          let modal = document.querySelector("#modal")
+          modal.querySelector("p").innerHTML = error
+          modal.classList.remove("hidden")
+
+          setTimeout(function(){modal.classList.add("hidden")},5000)
+        })
+    
+      e.preventDefault()
+    } else {
+      e.target.innerHTML = EMPTY_HEART
+      e.target.classList.remove("activated-heart")
+    }
+    
+  }))
+
+
+})
 
 
 //------------------------------------------------------------------------------
