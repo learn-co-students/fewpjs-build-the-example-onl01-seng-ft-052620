d28e0e5b7c27bacc74c456908bf3212df9b50b2b
sean-slaughter <sthompson2822@gmail.com>
2020-09-03 15:42:05 -0500

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,51 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//add hidden class to modal
+let errorModal = document.getElementById("modal");
+errorModal.classList.add("hidden");
+let errorMessage = document.getElementById("modal-message")
+
+//add event listener to hearts
+addListenerToHearts();
+
+
+function addListenerToHearts(){
+  likes = document.querySelectorAll(".like-glyph");
+  likes.forEach(function(like){
+    like.addEventListener("click", function(e){
+      if (e.target.innerHTML == EMPTY_HEART){
+        mimicServerCall()
+        .then(function(obj){
+          e.target.innerHTML = FULL_HEART
+          e.target.classList.add("activated-heart")
+        })
+        .catch(function(error){
+          console.log(error);
+          errorMessage.innerHTML = error;
+          errorModal.classList.remove("hidden");
+          setTimeout(() => errorModal.classList.add("hidden"), 5000);
+        })
+      }
+      else{
+        mimicServerCall()
+        .then(function(obj){
+          e.target.innerHTML = EMPTY_HEART
+          e.target.classList.remove("activated-heart")
+        })
+        .catch(function(error){
+          console.log(error);
+          errorMessage.innerHTML = error;
+          errorModal.classList.remove("hidden");
+          setTimeout(() => errorModal.classList.add("hidden"), 5000);
+        })
+      }
+    });
+  });
+}
+
+
+
 
 
 
