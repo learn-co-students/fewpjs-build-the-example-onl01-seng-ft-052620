0fb64b7cd27db4f4802329b65ab66d11c0d5b82f
barrettanderson <barrettanderson1989@gmail.com>
2020-09-03 19:42:48 -0700

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,39 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let errorStop = document.getElementById("modal")
+let likeClassSelect = document.getElementsByClassName("like")
 
+errorStop.className = "hidden"
 
+document.addEventListener("DOMContentLoaded", () => {
+})
+
+function heartToggle (heart) {
+  if (heart == EMPTY_HEART){
+    return FULL_HEART
+  }
+  else {return EMPTY_HEART}
+}
+
+for (let heart of likeClassSelect) {
+  heart.addEventListener("click", clickLike)
+}
+
+function clickLike (event) {
+  let heart = event.target
+  console.log(heart.innerText)
+  mimicServerCall()
+  .then(function () {
+    heart.innerText = heartToggle(heart.innerText)
+    if (heart.innerText == FULL_HEART)
+    {heart.className = "activated-heart"}
+    else { heart.className = "like-glyph"}
+  })
+  .catch(function () {
+  errorStop.className = ""
+  })
+}
 
 
 //------------------------------------------------------------------------------
